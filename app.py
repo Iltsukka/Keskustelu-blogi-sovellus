9ede7e7d8209bc0aa37da5bfcdbb5d192769b06f
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-09-14 19:33:06 +0300

    created simple login form

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
-
-@app.route('/')
+#tämä on vain testi poistan myöhemmin
+@app.route('/testi')
 def index():
     result = db.session.execute(text('SELECT content FROM messages'))
     messages = result.fetchall()
     return render_template('testi.html', count=len(messages), messages=messages)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
