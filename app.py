e850a13f36fabb8c50601b2ec35d16a7818a4cbe
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-09-14 19:06:20 +0300

    basic configurations and connection to database

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    result = db.session.execute(text('SELECT content FROM messages'))
+    messages = result.fetchall()
+    return render_template('testi.html', count=len(messages), messages=messages)
