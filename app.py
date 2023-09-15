551068ed02598b4e50d694105cfee2e9e935d94e
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-09-15 11:14:46 +0300

    template and route for creating new blogs, updated db schema

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def testi():
 
 @app.route('/')
 def index():
+    
     return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
@@ -47,15 +48,37 @@ def add_user():
     username = request.form['username']
     password = request.form['password']
     password2 = request.form['password2']
+    if password == '' or username == '':
+        return 'error template will be updated here'
+    if password != password2:
+        return 'some error'
     hash = generate_password_hash(password)
     sql = text('INSERT INTO users (username, password) VALUES (:username, :password)')
     db.session.execute(sql, {'username':username, 'password':hash})
     db.session.commit()
-    return 'success?'
+    session['username'] = username
+    return redirect('/')
 
 @app.route('/log_out', methods=['POST'])
 def log_out():
     del session['username']
     return redirect('/')
+
+@app.route('/blogform', methods=['POST'])
+def blogform():
+    return render_template('newblog.html')
+
+@app.route('/create_blog', methods=['POST'])
+def create_blog():
+    topic = request.form['topic']
+    username = session['username']
+    sql = text("INSERT INTO blogs (topic, username, time_of) VALUES (:topic, :username, NOW())")
+    if username:
+        db.session.execute(sql, {"topic":topic, "username":username})
+        db.session.commit()
+        return redirect('/')
+    else:
+        return 'error creating a blog'
+
     
     
--- schema.sql
+++ schema.sql
@@ -8,4 +8,12 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
     password TEXT
-)
+);
+
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    topic TEXT,
+    username TEXT,
+    time_of TIMESTAMP
+    
+);
